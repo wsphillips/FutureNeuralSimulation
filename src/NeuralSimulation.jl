12d9aceed098b3452f983906d635141906d13967
Wiktor Phillips <wsphillips@gmail.com>
2021-01-24 02:29:05 +0100

    .

--- src/NeuralSimulation.jl
+++ src/NeuralSimulation.jl
@@ -4,7 +4,7 @@ using DiffEqBase
 export CableSection, HHCableFunction, BEHHCable, HHCableProblem
 
 # Placeholder geometry type, this will be it's own major portion of the package/API eventually
-struct CableSection
+struct CableSection{T,N}
     radius::T
     len::T
     rl::T
@@ -27,19 +27,17 @@ abstract type AbstractNeuronalFunction{iip} <: DiffEqBase.AbstractDiffEqFunction
 
 # A NeuronalSystem containing equations should be rewritten into more efficient forms for certain methods.
 # The primary case being rewrites for solving backwards Euler in fewer steps (we don't have to do fixed-point iteration)
-struct HHCableFunction{iip, F1, F2, BEF1, BEF2, S} <: AbstractNeuronalFunction{iip}
-    f1::F1  # Voltage equation (du, u, p, t)
-    f2::F2  # Channel gating dynamics (du, u, p, t)
-    LDUsolve::BEF1 # Voltage backwards Euler tridiagonal solve (u, p, dt)
-    itrgates::BEF2 # Channel gates Backwards euler iterable approximation (u, p, dt)
-    # ignoring other typical fields used in DiffEq function types for now
+struct HHCableFunction{iip, F1, F2} <: AbstractNeuronalFunction{iip}
+    f1::F1  # in-place update of coefficients for Hines matrix
+    f2::F2  # in-place Channel gating dynamics
+    function HHCableFunction{iip}(f1, f2) where {iip}
+        new{iip, typeof(f1), typeof(f2)}(f1,f2)
+    end
 end
-
-function SimpleActiveCableFunction{iip, F1, F2, S}()
 
 abstract type AbstractNeuronalIntegrator{Alg, IIP, U, T} <: DiffEqBase.DEIntegrator{Alg, IIP, U, T} end
 
-mutable struct BackwardsEulerHHCableIntegrator{IIP, S, T, P, F} <: AbstractNeuronalIntegrator{SimpleActiveCable, IIP, S, T}
+mutable struct BackwardsEulerHHCableIntegrator{IIP, S, T, P, F, HinesMatrix} <: AbstractNeuronalIntegrator{BackwardsEuler, IIP, S, T}
     f::F     # equations
     uprev::S # Previous state
     u::S     # Current state 
@@ -48,22 +46,27 @@ mutable struct BackwardsEulerHHCableIntegrator{IIP, S, T, P, F} <: AbstractNeuro
     t0::T    # Initial time step, only for re-initialization
     dt::T    # Step size
     p::P     # Parameters container
+    H::HinesMatrix # Hines matrix
+    function BackwardsEulerHHCableIntegrator(f::HHCableFunction, uprev, u, tprev, t, t0, dt, p, H)
+        new{isinplace(f), typeof(u), typeof(t), typeof(p), typeof(f), typeof(H)}(
+            f, uprev, u, tprev, t, t0, dt, p, H
+        )
+    end
 end
 
 const BEHHCable = BackwardsEulerHHCableIntegrator
+DiffEqBase.isinplace(::HHCableFunction{IIP}) where {IIP} = IIP
 DiffEqBase.isinplace(::BEHHCable{IIP}) where {IIP} = IIP
 
 abstract type AbstractNeuronalProblem{uType, tType, isinplace} <: DiffEqBase.DEProblem end
 
-struct HHCableProblem{uType, tType, isinplace, P, F, K} <: AbstractNeuronalProblem{uType, tType, isinplace}
+struct HHCableProblem{uType, tType, isinplace, P, F} <: AbstractNeuronalProblem{uType, tType, isinplace}
     f::F
     u0::uType
     tspan::tType
     p::P # constants to be supplied as second arg of `f`
-    kwargs::K # DiffEq says this is `a callback to be applied to every solver which uses the problem`
-              # I'm not sure what's meant by that atm
-    function HHCableProblem{iip}(f::AbstractNeuronalFunction{iip}, u0, tspan, p; kwargs...) where {iip}
-        new{typeof(u0), typeof(tspan), isinplace(f), typeof(p), typeof(f), typeof(kwargs)}(f, u0, tspan, p, kwargs)
+    function HHCableProblem(f::AbstractNeuronalFunction, u0, tspan, p)
+        new{typeof(u0), typeof(tspan), DiffEqBase.isinplace(f), typeof(p), typeof(f)}(f, u0, tspan, p)
     end
 end
 
@@ -71,6 +74,10 @@ end
 
 function DiffEqBase.solve(prob::AbstractNeuronalProblem, args...; kwargs...)
     __solve(prob,args...; kwargs...)
+end
+
+function DiffEqBase.init(prob::AbstractNeuronalProblem, args...; kwargs...)
+    __init(prob,args...; kwargs...)
 end
 
 function DiffEqBase.__init(prob::HHCableProblem, alg::BackwardsEuler; dt = error("dt is required for this algorithm"))
@@ -97,7 +104,7 @@ function DiffEqBase.__solve(prob::HHCableProblem, alg::BackwardsEuler; dt = erro
         us[j] = similar(u0)
     end
     # construct integrator (holds pre-allocated state/parameter data needed for each step)
-    integ = hhcable_init(prob.f, DiffEqBase.isinplace(prob), prob.u0, prob.tspan[1], dt, prob.p)
+    integ = hhcable_init(prob.f, #=DiffEqBase.isinplace(prob)=# true, prob.u0, prob.tspan[1], dt, prob.p)
 
     # step through time series and copy new state vector to history (`us`) each step
     for i in 2:n
@@ -118,26 +125,49 @@ end
     return integ
 end
 
+# Solves an unbranched cable (tridiagonal matrix); needs modification for generalization to branched cable case
+# In branched structures, references to the parent compartment (idx - 1) are substituted with a lookup table
+# NOTE: This solver is independent of the DE method (its just slightly-modified LAPACK--see `dgtsv()`)
+function thomas!(Hines, V)
+    #Backward pass
+    for i in N:-1:2
+        factor = Hines.U[i] / Hines.D[i]
+        Hines.D[i-1] -= factor * Hines.L[i] 
+        V[i-1] -= factor * V[i] 
+    end
+
+    # Root solve
+    V[1] /= Hines.D[1]
+
+    # Forward pass
+    for j in 2:N
+        V[j] -= Hines.L[j]*V[j-1]
+        V[j] /= Hines.D[j]
+    end
+
+    return nothing
+end
+
 # Stepping
 # In-place stepping
-
 @inline function DiffEqBase.step!(integ::BEHHCable{true, S, T}) where {T, S}
     
-    integ.uprev       .= integ.u
-    stepvoltage!       = integ.f1
-    stepgates!         = integ.f2
-    p                  = integ.p
-    t                  = integ.t
-    dt                 = integ.dt
-    u                  = integ.u
+    integ.uprev        .= integ.u
+    f                   = integ.f
+    p                   = integ.p
+    t                   = integ.t
+    dt                  = integ.dt
+    u                   = integ.u
+    update_hines!       = f.f_hines
 
     integ.tprev = t
     integ.t += dt
 
-    # Update membrane voltage
-    stepvoltage!(du, u, p, t)
+    hines!(Hines, u, p, dt)
+    # Solve Hines for membrane voltage
+    thomas!(Hines, view(u, :, 1))
     # Update dimensionless channel gating vars
-    stepgates!(du, u, p, t)
+    stepgates!(u, p, t)
 
     return nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test
-using NeuronalSimulation
+using NeuralSimulation
 
 # Hodgkin-Huxley channel gating functions
 αn(V) = V == -55.0 ? 0.1 : (0.01*(V + 55))/(1 - exp(-(V + 55)/10))
@@ -14,8 +14,22 @@ m∞(V) = αm(V)/(αm(V) + βm(V))
 h∞(V) = αh(V)/(αh(V) + βh(V))
 n∞(V) = αn(V)/(αn(V) + βn(V))
 
-function stepgates!(u, p, dt)
+# Updating gates (generic version)
+function gates!(du, u, p, t)
     V, m, h, n = [view(u, :, x) for x in 1:4]
+    _, dm, dh, dn = [view(du, :, x) for x in 1:4]
+
+    for i in 1:length(V)
+        dm[i] = αm(V[i]) * (1 - m[i]) - βm(V[i])*m[i]
+        dh[i] = αh(V[i]) * (1 - h[i]) - βh(V[i])*h[i]
+        dn[i] = αn(V[i]) * (1 - n[i]) - βn(V[i])*n[i]
+    end
+end
+
+# Updating gates (rewritten for backwards euler)
+function BEgates!(u, p, dt)
+    V, m, h, n = [view(u, :, x) for x in 1:4]
+
     for i in 1:length(V)
         m[i] = (m[i] + αm(V[i])*dt)/(1 + (αm(V[i]) + βm(V[i]))*dt)
         h[i] = (h[i] + αh(V[i])*dt)/(1 + (αh(V[i]) + βh(V[i]))*dt)
@@ -23,17 +37,18 @@ function stepgates!(u, p, dt)
     end
 end
 
-# Eventually we want to use ModelingToolkit IR to build a function from a `ModelingToolkit.Equation`
-# If we pass a keyword, like `HinesBackwardsEuler=true` it should generate a function that spits out
-# updated coefficients for the tridiagonal solve of the cable equation 
-
-mutable struct HinesMatrix
+# TODO: Hines matrix prototype -> should use ModelingToolkit IR to build a function from
+# a `ModelingToolkit.Equation` If we pass a keyword, like `HinesBackwardsEuler=true` it
+# should generate a function that spits out updated coefficients for the tridiagonal solve
+# of the cable equation 
+mutable struct HinesMatrix{S}
     L::S
     D::S
     U::S
 end
 
-# Eventually other constructors for more elaborate tree structure types
+# TODO: other constructors for more elaborate tree structure types
+# this version is specific to backwards Euler
 function HinesMatrix(cable::CableSection, dt)
         rl = cable.rl
         a = cable.radius
@@ -42,8 +57,8 @@ function HinesMatrix(cable::CableSection, dt)
         dx = cable.len/n
 
         L = Vector{typeof(cable.len)}(undef, n)
-        D = copy(L)
-        U = copy(L)
+        D = similar(L)
+        U = similar(L)
 
         # L and U are static
         L[1] = 0.0
@@ -54,40 +69,55 @@ function HinesMatrix(cable::CableSection, dt)
         U[2:end-1] .= -(a*dt)/(2*rl*cm*dx^2)
         U[end] = 0.0
 
-        new(L, D, U)
+        return HinesMatrix(L, D, U)
 end
 
 # In-place update of the Hines coefficients + RHS (`u` -> Vm)
 function f_hines!(Hines, u, p, dt)
     V, m, h, n = [view(u, :, x) for x in 1:4]
-    
-    # FIXME: these parameters need to be setup correctly
     a, rl, cm, gna, gk, gl, Ena, Ek, El, Iapp, dx = p
-
     for k in 1:length(V)
         Hines.D[k] = 1 + (a*dt)/(rl*cm*dx^2) + (dt/cm)*(gna*m[k]^3*h[k] + gk*n[k]^4 + gl)
-        V[k] = V[k] + (dt/cm)*(gna*m[k]^3*h[k]*Ena + gk*n[k]^4*Ek + gl*El)
+        V[k] += (dt/cm)*(gna*m[k]^3*h[k]*Ena + gk*n[k]^4*Ek + gl*El)
     end 
-
     # in first compartment we inject additional 1 nA current
     # (for testing only; stimulation should be specified at high level)
     V[1] += (Iapp*dt)/(pi*a*cm*dx)
-    
-    #Backward pass
-    for i in N:-1:2
-        factor = Hines.U[i] / Hines.D[i]
-        Hines.D[i-1] -= factor * Hines.L[i] 
-        V[i-1] -= factor * V[i] 
-    end
 
-    # Root solve
-    V[1] /= Hines.D[1]
-
-    # Forward pass
-    for j in 2:N
-        V[j] -= Hines.L[j]*V[j-1]
-        V[j] /= Hines.D[j]
-    end
+    return nothing
 end
 
+##############################################################
 
+dt = 0.025 # ms
+Iapp = 1.0e-6 # mA (1 nA)
+cable_length = 2000.0e-3 # 2000 µm length
+N = 2000
+dx = cable_length/N
+radius = 2.0e-3 # µm in mm
+area = 2*pi*a*dx
+
+rl = 1.0e6 # 1 kΩ in milliohms*mm
+gl  = 0.003 #mS/mm^2
+gna = 1.2 # mS/mm^2 (modified from 1.2 mS)
+gk  = .36 # mS/mm^2
+El  = -54.4 # mV
+Ena = 50.0 # mV
+Ek  = -77.0 # mV
+cm  = 1.0e-5 # mF/mm^2
+
+Vinit = -55.0 # mV
+
+p = [radius, rl, cm, gna, gk, gl, Ena, Ek, El, Iapp, dx]
+
+foo = CableSection(radius, cable_length, rl, cm, N)
+Hmat = HinesMatrix(foo, 0.025)
+
+u0 = zeros(2000,4)
+
+u0[:,1] .= Vinit
+u0[:,2] .= m∞(Vinit)
+u0[:,3] .= h∞(Vinit)
+u0[:,4] .= n∞(Vinit)
+
+fn = HHCableFunction(f_hines!, BEgates!)
